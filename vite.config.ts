70f066c4ce9d845d20d91964eeb1f85bf0fcc94e
Валентин <astrotrain55@gmail.com>
2024-02-04 21:59:18 +0500

    clean

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
+  publicDir: false,
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: './src/lib.ts',
+      name: 'react-bitrix24',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
