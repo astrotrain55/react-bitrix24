92e7a980a0dfed8abd5ae54514f9bb608f2577b1
Валентин <astrotrain55@gmail.com>
2024-06-12 17:27:14 +0500

    bx-progressbar

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,6 @@ export { BxAlert } from './components/BxAlert';
 export { BxButton } from './components/BxButton';
 export { BxCheckbox } from './components/BxCheckbox';
 export { BxIcon } from './components/BxIcon';
+export { BxProgressbar } from './components/BxProgressbar';
 export { BxRadio } from './components/BxRadio';
 export { loadStyles } from './utils/loadStyles';
