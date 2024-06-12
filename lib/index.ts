411fc592f215f369650cdaa5510ebcb22b8402b8
Валентин <astrotrain55@gmail.com>
2024-06-12 16:57:38 +0500

    bx-icon

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 export { BxAlert } from './components/BxAlert';
 export { BxButton } from './components/BxButton';
 export { BxCheckbox } from './components/BxCheckbox';
+export { BxIcon } from './components/BxIcon';
 export { BxRadio } from './components/BxRadio';
 export { loadStyles } from './utils/loadStyles';
