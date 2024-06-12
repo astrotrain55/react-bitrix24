4fe9174778c42c6cd98b295df6c4ba386f1fc3f7
Валентин <astrotrain55@gmail.com>
2024-06-12 17:50:43 +0500

    bx-textarea

--- lib/index.ts
+++ lib/index.ts
@@ -4,4 +4,5 @@ export { BxCheckbox } from './components/BxCheckbox';
 export { BxIcon } from './components/BxIcon';
 export { BxProgressbar } from './components/BxProgressbar';
 export { BxRadio } from './components/BxRadio';
+export { BxTextarea } from './components/BxTextarea';
 export { loadStyles } from './utils/loadStyles';
